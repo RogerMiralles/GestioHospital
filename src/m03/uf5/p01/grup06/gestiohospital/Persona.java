2d4a03c31b508ef1bd6f6f816f694d3a2cb81ab5
Joel Caso Blanca <joel1999caso@gmail.com>
2018-12-03 09:21:26 +0100

    Inicio 2

---
+++ src/m03/uf5/p01/grup06/gestiohospital/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m03.uf5.p01.grup06.gestiohospital;
+
+/**
+ *
+ * @author Joel C
+ */
+public abstract class Persona {
+    
+    private String nom;
+    private String cognom1;
+    private String cognom2;
+    private String numSegSocial;
+    private String nif;
+    private String telefon;
+    private Adreca adreca;
+    
+    
+    
+}
