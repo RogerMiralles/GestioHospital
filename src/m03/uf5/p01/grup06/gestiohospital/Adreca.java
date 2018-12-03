d6c52084207dc769d52f7e41554e7b97af792f85
boucatalan <davidbou1996@gmail.com>
2018-12-03 10:05:41 +0100

    Adreça, clase con constructor.

--- src/m03/uf5/p01/grup06/gestiohospital/Adreca.java
+++ src/m03/uf5/p01/grup06/gestiohospital/Adreca.java
@@ -10,5 +10,19 @@ package m03.uf5.p01.grup06.gestiohospital;
  * @author Joel C
  */
 public class Adreca {
+    private String ciutat;
+    private int codiPostal;
+    private String carrer;
+    private int numero;
+    private String planta;
+    private String porta;
     
+    Adreca(String ciutat, int codiPostal, String carrer, int numero, String planta, String porta){
+        this.ciutat = ciutat;
+        this.codiPostal = codiPostal;
+        this.carrer = carrer;
+        this.numero = numero;
+        this.planta = planta;
+        this.porta = porta;
+    }
 }
