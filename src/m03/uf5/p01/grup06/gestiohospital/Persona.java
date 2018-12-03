f8555c2b66ad76c6cb4f5f53b2b84317da54be27
Joel Caso Blanca <joel1999caso@gmail.com>
2018-12-03 09:37:31 +0100

    Persona v1

--- src/m03/uf5/p01/grup06/gestiohospital/Persona.java
+++ src/m03/uf5/p01/grup06/gestiohospital/Persona.java
@@ -5,6 +5,9 @@
  */
 package m03.uf5.p01.grup06.gestiohospital;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author Joel C
@@ -19,6 +22,33 @@ public abstract class Persona {
     private String telefon;
     private Adreca adreca;
     
+    public Persona (String nom, String cognom1, String cognom2, String numSegSocial, String nif, String telefon, Adreca Adreca) {
+        
+        
+    }
     
-    
+    private boolean nifCorrecto (String nif) {
+        String TABLA_LETRA ="TRWAGMYFPDXBNJZSQVHLCKE";        
+        Pattern reglas = Pattern.compile("[0-9]{8}[A-Z]");
+        Matcher textAnalitzar;
+        boolean valido = false;
+        
+        for (int i = 0; i < 5 && !valido; i++) {
+            textAnalitzar = reglas.matcher(nif);
+            if(!textAnalitzar.matches())
+                return false;
+            else{
+                Pattern reglaDni = Pattern.compile("[0-9]{8}");
+                Matcher textDni = reglaDni.matcher(nif);
+                textDni.find();
+                int dni = Integer.parseInt(textDni.group());
+                char lletra = TABLA_LETRA.charAt(dni % 23);
+                if(nif.charAt(nif.length()-1) == lletra)
+                    valido = true;
+                else 
+                    return false;
+            }
+        }
+        return true;
+    }       
 }
