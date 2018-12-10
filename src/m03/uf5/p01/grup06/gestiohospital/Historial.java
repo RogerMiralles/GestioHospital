69b22f7f2133d9c08e78b50feaa04d96b5486b41
Joel Caso Blanca <joel1999caso@gmail.com>
2018-12-10 09:54:58 +0100

    Historial fin

---
+++ src/m03/uf5/p01/grup06/gestiohospital/Historial.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m03.uf5.p01.grup06.gestiohospital;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Joel C
+ */
+public class Historial {
+    
+    private final int codi;
+    private static int ultCodigo = 0;
+    private ArrayList<Visita> visitas = new ArrayList<>();
+    
+    public Historial() {
+        this.codi = ++ultCodigo;
+    }
+    
+    @Override
+    public String toString() {
+        String s = "HISTORIAL " + this.codi + "\n----------------------------------------------";
+        
+        Iterator<Visita> lit = visitas.iterator();
+        
+        while (lit.hasNext()) {
+            Visita v = lit.next();
+            s += "\n Visita " + v.getData().toString();
+            s += "\n Metge: " + v.getMetge().getNom() + " " + v.getMetge().getCognom1();
+            s += "\n Malaltia: + " + v.getMalaltia().toString();
+            s += "\n----------------------------------------------";
+        }
+        return s;
+    }
+}
