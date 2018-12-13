74166d1279d6c9c056f6a85ec2e9636327258844
Joel Caso Blanca <joel1999caso@gmail.com>
2018-12-13 09:19:45 +0100

    Hospital v1

--- src/m03/uf5/p01/grup06/gestiohospital/Hospital.java
+++ src/m03/uf5/p01/grup06/gestiohospital/Hospital.java
@@ -1,29 +1,35 @@
 package m03.uf5.p01.grup06.gestiohospital;
 
-
+import java.util.HashMap;
 import java.util.Map;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Aleix
- */
 public class Hospital {
     private Map<String, Pacient> pacients;
     private Map<String, Metge> metges;
     private Map<Integer, Historial> historials;
     private Map<Integer, Malaltia> malalties;
-    private String nomHospital;
+    private final String nomHospital;
     private Adreca adreca;
     
     
-    public Hospital(){
-        
+    public Hospital(String nom, Adreca adreca){
+        this.nomHospital = nom;
+        this.adreca = adreca;
+        this.pacients = new HashMap<>();
+        this.metges = new HashMap<>();
+        this.historials = new HashMap<>();
+        this.malalties = new HashMap<>();
     }
+    
+    public Hospital(String nom, String ciutat, int codiPostal, String carrer, int numero, String planta, String porta) {
+        this.nomHospital = nom;
+        this.adreca = new Adreca(ciutat, codiPostal, carrer, numero, planta, porta);
+        this.pacients = new HashMap<>();
+        this.metges = new HashMap<>();
+        this.historials = new HashMap<>();
+        this.malalties = new HashMap<>();
+    }
+    
+    public boolean addMetge ()
     
 }
